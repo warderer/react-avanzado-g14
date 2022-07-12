d8becd1cfb6c292ae7ade5ffaabda8d18b02f703
César Guerra <warderer@gmail.com>
2022-07-11 21:35:58 -0500

    ✨ SongList con Contexto

--- 03-react-context/src/components/SongList/SongList.jsx
+++ 03-react-context/src/components/SongList/SongList.jsx
@@ -1,27 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useSongContext } from '@/context/SongContext'
 import './songlist.css'
 
-// Usamos @ para no tener que poner ../../
-// import canciones from '../../assets/listaCanciones.json'
-import canciones from '@/assets/listaCanciones.json'
-
 const SongList = () => {
-  const [list, setList] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  // Simulo la llamada a la API de la lista de canciones
-  useEffect(() => {
-    setTimeout(() => {
-      setList(canciones)
-      setLoading(false)
-    }, [2000])
-  }, [])
+  const context = useSongContext()
 
   return (
     <section>
-      {loading
+      {context.loading
         ? <h1>Cargando...</h1>
-        : list.map((song) => (
+        : context.list.map((song) => (
           <div className='row-song' key={song.id}>
             <h4>{song.title}</h4>
             <p>{song.artist}</p>
