a34f7cb6011274cc23bac18212658db46f9ced1c
César Guerra <warderer@gmail.com>
2022-07-12 19:20:35 -0500

    ✨ SongDetails funcionando con Estado Global

--- 03-react-context/src/components/songDetails/SongDetails.jsx
+++ 03-react-context/src/components/songDetails/SongDetails.jsx
@@ -1,8 +1,19 @@
+import { useSongContext } from '@/context/SongContext'
+
 const SongDetails = () => {
+  // Consumo mi estado global
+  const context = useSongContext()
   return (
-    <div>
-      <h1>El detalle de la canción que seleccionaste</h1>
-    </div>
+    <>
+      {context.selectedSong.title
+        ? <div>
+          <img src={`https://picsum.photos/seed/${context.selectedSong.id}/400/400`} alt='Portada del Disco' />
+          <h1>{context.selectedSong.title}</h1>
+          <h4>{context.selectedSong.artist}</h4>
+          <h6>{context.selectedSong.year}</h6>
+        </div>
+        : <h1>Selecciona una canción </h1>}
+    </>
   )
 }
 
