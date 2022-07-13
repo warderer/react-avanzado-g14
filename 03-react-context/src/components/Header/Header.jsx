1861b435e507931e4bdd686863af557bca0a195e
César Guerra <warderer@gmail.com>
2022-07-12 19:44:59 -0500

    ✨ Header con Next y Prev de Canciones

---
+++ 03-react-context/src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import { useSongContext } from '@/context/SongContext'
+
+const Header = () => {
+  const context = useSongContext()
+  const song = context.selectedSong
+
+  const nextSong = () => {
+    const songIndex = context.list.findIndex(s => s.id === song.id)
+    context.list.length > songIndex + 1
+      ? context.setSelectedSong(context.list[songIndex + 1])
+      : context.setSelectedSong(context.list[0])
+  }
+
+  const prevSong = () => {
+    const songIndex = context.list.findIndex(s => s.id === song.id)
+    songIndex > 0
+      ? context.setSelectedSong(context.list[songIndex - 1])
+      : context.setSelectedSong(context.list[context.list.length - 1])
+  }
+
+  return (
+    song.title
+      ? (
+        <div>Now Playing: {song.title} - {song.artist}
+          <button onClick={prevSong}> Prev </button>
+          <button onClick={nextSong}> Next </button>
+        </div>
+        )
+      : <div>Select a Song</div>
+  )
+}
+
+export default Header
--- 03-react-context/src/pages/Home/Home.jsx
+++ 03-react-context/src/pages/Home/Home.jsx
@@ -1,22 +1,26 @@
 import './home.css'
 import SongList from '@/components/SongList'
 import SongDetails from '@/components/SongDetails'
+import Header from '@/components/Header'
 import { SongProvider } from '@/context/SongContext'
 
 const Home = () => {
   return (
-    <div className='home-container'>
+    <>
       <SongProvider>
-        <div className='izquierdo'>
-          <h2>Lado Izquierdo</h2>
-          <SongList />
-        </div>
-        <div className='derecho'>
-          <h2>Lado Derecho</h2>
-          <SongDetails />
+        <Header />
+        <div className='home-container'>
+          <div className='izquierdo'>
+            <h2>Lado Izquierdo</h2>
+            <SongList />
+          </div>
+          <div className='derecho'>
+            <h2>Lado Derecho</h2>
+            <SongDetails />
+          </div>
         </div>
       </SongProvider>
-    </div>
+    </>
   )
 }
 
