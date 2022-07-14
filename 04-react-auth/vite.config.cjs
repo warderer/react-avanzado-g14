d8dc5470ea8b2218457af334b1802c9a8da3f7e9
César Guerra <warderer@gmail.com>
2022-07-13 21:58:30 -0500

    🚑 Renombrar config de vite para v3

--- 04-react-auth/vite.config.cjs
+++ 04-react-auth/vite.config.cjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path' // #1 Añado el Path
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // #2 Configuraba el Alias para mis carpetas
+  // Configuro Alias para mis Carpetas
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
