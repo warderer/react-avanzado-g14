f90e0f7afab216e613ba90324e59d7e22d69e89e
César Guerra <warderer@gmail.com>
2022-07-20 21:23:14 -0500

    ✨ Secret como ruta protegida

--- 04-react-auth/src/routes/index.jsx
+++ 04-react-auth/src/routes/index.jsx
@@ -1,13 +1,24 @@
+import { useContext } from 'react'
+import { AuthContext } from '@/context/AuthContext'
 import { Routes, Route } from 'react-router-dom'
 import { Home, Login, Secret, Signup, Logout } from '@/pages'
+import Protected from '@/hoc/Protected'
 
 const RoutesIndex = () => {
+  const { isAuth } = useContext(AuthContext)
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
-      <Route path='/secret' element={<Secret />} />
+      <Route
+        path='/secret'
+        element={
+          <Protected isLoggedIn={isAuth}>
+            <Secret />
+          </Protected>
+      }
+      />
       <Route path='/logout' element={<Logout />} />
     </Routes>
   )
