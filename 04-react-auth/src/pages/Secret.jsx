d3400ec70cf107c2f2eeb87f37d27383ecf31b3e
César Guerra <warderer@gmail.com>
2022-07-20 21:34:10 -0500

    ✨ Secret con Renderizado Condicional

--- 04-react-auth/src/pages/Secret.jsx
+++ 04-react-auth/src/pages/Secret.jsx
@@ -1,6 +1,25 @@
+import { useContext } from 'react'
+import { AuthContext } from '@/context/AuthContext'
+
 const Secret = () => {
+  const { user } = useContext(AuthContext)
   return (
-    <div>Secret</div>
+    <div>
+      <h1>Secret</h1>
+      {
+        user.role === 'ADMIN'
+          ? <h2>Hola Admin</h2>
+          : <h2>Hola Customer</h2>
+      }
+
+      {
+        user.role === 'CUSTOMER' && <h4>Bienvenido seas Cliente</h4>
+      }
+
+      {
+        user.role === 'ADMIN' && <h4>Bienvenido seas Admin</h4>
+      }
+    </div>
   )
 }
 
