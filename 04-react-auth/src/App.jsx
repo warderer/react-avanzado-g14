245da1cdda396f3092cec21cf2bb772a4b96ff91
César Guerra <warderer@gmail.com>
2022-07-20 20:31:07 -0500

    ✨ Implementación de Context Provider en App

--- 04-react-auth/src/App.jsx
+++ 04-react-auth/src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
 import RoutesIndex from './routes'
+import AuthContextProvider from '@/context/AuthContext'
 import './App.css'
 
 function App () {
   return (
     <>
-      <Router>
-        <Navbar />
-        <RoutesIndex />
-      </Router>
+      <AuthContextProvider>
+        <Router>
+          <Navbar />
+          <RoutesIndex />
+        </Router>
+      </AuthContextProvider>
     </>
   )
 }
