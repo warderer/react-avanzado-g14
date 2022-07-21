a8e996b99ec055c3a63d87b1d49357798241e3d2
César Guerra <warderer@gmail.com>
2022-07-20 19:21:01 -0500

    ✨ LogOut básico con LocalStorage

--- 04-react-auth/src/routes/index.jsx
+++ 04-react-auth/src/routes/index.jsx
@@ -1,5 +1,11 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { Home, Login, Secret, Signup } from '@/pages'
+
+// Componente que hara Logout
+const Logout = () => {
+  window.localStorage.removeItem('token')
+  return <Navigate to='/' />
+}
 
 const RoutesIndex = () => {
   return (
@@ -8,6 +14,7 @@ const RoutesIndex = () => {
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
       <Route path='/secret' element={<Secret />} />
+      <Route path='/logout' element={<Logout />} />
     </Routes>
   )
 }
